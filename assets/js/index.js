2c4908a562e1a306e12d6ba2995d924fc945f695
kemmie <kemmie.xd@gmail.com>
2018-11-05 22:27:00 +0700

    thai commit 22h30 5/11

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,10 @@ $('.slider-carousel').owlCarousel({
     autoplay: true,
     autoplayTimeout: 3000,
     autoplayHoverPause: true,
-    dots:true,
-    nav:false,
+    dots: true,
+    nav: false,
     navText: ["<i class='fa fa-long-arrow-left' aria-hidden='true'></i>","<i class='fa fa-long-arrow-right' aria-hidden='true'></i>" ] ,
-    autoplaySpeed:1000,
+    autoplaySpeed: 1000,
     responsive: {
         0: {
             items:1
@@ -18,4 +18,30 @@ $('.slider-carousel').owlCarousel({
             items:1
         }
     }
-})
+});
+
+$('document').ready(function() {
+    var url = window.location.href; 
+    $(".main-menu a").each(function() {
+        if(url == (this.href)) { 
+            $(this).closest("li a").addClass("active");
+        }
+    });
+
+    $('.toggleMenu').click(function() {
+        $(this).css('display','none');
+        $('.nav').css('transform','translateX(0)');
+        $('.menu-close').css('display','block');
+    });
+
+    $('.menu-close').click(function() {
+        $(this).css('display','none');
+        $('.toggleMenu').css('display','block');
+        $('.nav').css('transform','translateX(-150%)');
+    });
+
+    $('.nav li i').click(function() {
+        $('.nav li ul').css('position','relative').css('display','block')
+    });
+});
+
